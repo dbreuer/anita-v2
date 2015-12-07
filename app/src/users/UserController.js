49d23cb570fbe5e84e88fab8ff964d8d5e4586b5
David Breuer <dbreuer83@gmail.com>
2015-12-07 17:01:03 +0000

    Anita v2 init

--- app/src/users/UserController.js
+++ app/src/users/UserController.js
@@ -91,6 +91,51 @@
         }
     }
 
+
+      this.tiles = buildGridModel({
+          icon : "avatar:svg-",
+          title: "Svg-",
+          background: "",
+          type: '',
+          time: 0
+      });
+      function buildGridModel(tileTmpl){
+          var it, results = [ ];
+          for (var j=0; j<11; j++) {
+              it = angular.extend({},tileTmpl);
+              it.icon  = it.icon + (j+1);
+              it.title = it.title + (j+1);
+              it.span  = { row : 1, col : 1 };
+              switch(j+1) {
+                  case 1:
+                      it.background = "red";
+                      it.span.row = it.span.col = 2;
+                      it.weather = 'london';
+                      break;
+                  case 2: it.background = "green"; it.type = 'image';        break;
+                  case 3: it.background = "darkBlue"; it.type = 'image';      break;
+                  case 4:
+                      it.background = "blue";
+                      it.span.col = 2;
+                      it.type = 'timer';
+                      it.time = Math.round(+new Date("2015-12-19"));
+                      break;
+                  case 5:
+                      it.background = "yellow";
+                      it.span.row = it.span.col = 2;
+                      it.weather = 'budapest';
+                      break;
+                  case 6: it.background = "pink"; it.type = 'image';          break;
+                  case 7: it.background = "darkBlue"; it.type = 'image';      break;
+                  case 8: it.background = "purple"; it.type = 'image';       break;
+                  case 9: it.background = "deepBlue";it.type = 'image';      break;
+                  case 10: it.background = "lightPurple";it.type = 'image';  break;
+                  case 11: it.background = "yellow"; it.type = 'image';      break;
+              }
+              results.push(it);
+          }
+          return results;
+      }
   }
 
 })();
--- app/src/users/Users.js
+++ app/src/users/Users.js
@@ -2,7 +2,20 @@
   'use strict';
 
   // Prepare the 'users' module for subsequent registration of controllers and delegates
-  angular.module('users', [ 'ngMaterial' ]);
+  angular.module('users', [ 'ngMaterial', 'timer' ])
+  .config( function( $mdIconProvider ){
+    $mdIconProvider.iconSet("avatar", 'assets/svg/avatars.svg', 128);
+  })
+      .directive('weather', weatherDirective);
 
+  function weatherDirective() {
+    return {
+      restrict: 'AE',
+      repalce: true,
+      link: function(){
+
+      }
+    }
+  }
 
 })();
