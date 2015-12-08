eca3d2f9c9c95a70726ddde8193091875f44d68a
David Breuer <dbreuer83@gmail.com>
2015-12-08 12:02:59 +0000

    rebuild

--- web.js
+++ web.js
@@ -13,7 +13,7 @@ app.set('port', (process.env.PORT || 5000));
 
 
 app.use(express.static(__dirname + '/app'));
-app.use('/bower_components',  express.static(__dirname + '/app'));
+app.use('/bower_components',  express.static(__dirname + '/app/bower_components'));
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
