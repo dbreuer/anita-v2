93bb655964595f36c97b29e742d5ffdff6e88b07
David Breuer <dbreuer83@gmail.com>
2015-12-08 11:50:14 +0000

    component static access

--- web.js
+++ web.js
@@ -9,9 +9,10 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/app'))
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.set('port', (process.env.PORT || 5000));
+
+app.use('/bower_components',  express.static(__dirname + '/app/bower_components'));
+app.use(express.static(__dirname + '/app'));
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
